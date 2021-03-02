b34daf1e22e9d75bebf0cfc843a78ffe72bf7ba9
Zhantay8 <700831275bekbo>
2021-03-02 20:02:04 +0600

    initialized project

---
+++ src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+
+        Scanner sc = new Scanner(System.in);
+/*        Scanner tekct =new Scanner(System.in);
+        System.out.println("Hello world");
+        System.out.println("Жантай байке");
+        int n = 5;
+        System.out.println(n);
+
+        System.out.println("java ты супер язык !");
+        System.out.println("5");
+
+
+        int x = 10;
+        int y = 25;
+        int z = x + y;
+        System.out.println("x+y=" + z);
+
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int sum = c + d;
+        System.out.println("sum=" + sum);
+
+        String tek=tekct.nextLine();
+        System.out.println(tek);              */
+
+
+        //  пример 1
+
+  /*   Scanner tekct = new Scanner(System.in);
+     System.out.print(("ваше имя ?"));
+     String name = tekct.nextLine();
+     System.out.println(name);
+     System.out.println("ваш возраст ");
+     int voz = sc.nextInt();
+     System.out.println(voz+" это хорошо");
+     System.out.println("Ваша зарплата ?");
+     float zarplata = sc.nextFloat();
+     System.out.print("%.2f\n", zarplata);
+     */
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
